35093d344cd3aa84bf1d96de991bd346a7ef69b5
Nuno Leite <nunocsleite@gmail.com>
2020-03-02 15:24:06 +0000

    Aula-01-02

---
+++ Semana 2/Aula 01-02-ReflectionExample/ReflectionExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Reflection;
+
+namespace ReflectionExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = "Ola";
+            System.String s1 = "Ola";
+
+            Type t = s.GetType();
+
+            Console.WriteLine("type name = {0}", t.Name);
+            MethodInfo[] mis = t.GetMethods(); // Get public methods
+            foreach (MethodInfo mi in mis)
+            {
+                //Console.WriteLine("current method info = {0}", mi.Name);
+                Console.WriteLine("current method info = {0}", mi);
+
+            }
+        }
+    }
+}
