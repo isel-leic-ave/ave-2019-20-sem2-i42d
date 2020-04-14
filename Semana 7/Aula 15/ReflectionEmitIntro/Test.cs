62c8afa21f652a1bc58fa6813f353f44c5543bed
Nuno Leite <nunocsleite@gmail.com>
2020-04-14 16:34:59 +0100

    Week 7 - Lecture 15

---
+++ Semana 7/Aula 15/ReflectionEmitIntro/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ReflectionEmitIntro
+{
+
+    //
+    // Exemplo Hard-coded
+    // 1. Fazer exemplo hard-coded em C#
+    // Compilar em Modo Release
+    // 2. ILDASM
+    // 3. Traduzir para chamadas ...Emit o programa IL hard-coded
+    //
+    public class Test
+    {
+        public static void Main1(string[] args)
+        {
+            Console.WriteLine("Ola {0}", 10);
+
+        }
+    }
+}
