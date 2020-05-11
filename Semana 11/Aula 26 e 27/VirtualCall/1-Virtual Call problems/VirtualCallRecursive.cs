4d9b48849b4612dfa851ce182c492827512b5982
Nuno Leite <nunocsleite@gmail.com>
2020-05-11 16:36:07 +0100

    Week 11 - Lecture 26-27

---
+++ Semana 11/Aula 26 e 27/VirtualCall/1-Virtual Call problems/VirtualCallRecursive.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Aula27.VirtualCallproblems
+{
+    public class VirtualCallRecursive
+    {
+        public VirtualCallRecursive()
+        {
+        }
+
+        public override bool Equals(object o) {
+            return base.Equals(o); // Tipo da referência: Object, pois base.Equals(...)
+            // converte this para referência para tipo base (Object).
+            // Regra: Equals em Object é virtual? Sim, então callvirt.
+            // Tipo exato da referência "this": (olhando para o Main) é um objecto do 
+            // tipo VirtualCallRecursive.
+            // callvirt realiza despacho dinâmico para a tabela de métodos de 
+            // VirtualCallRecursive, invoca Equals de VirtualCallRecursive => chamada recursiva!
+            //
+            // Para evitar chamada recursiva infinita, é gerado um call em vez de callvirt
+            // => despacho estático
+            //
+            // IL code:
+            // .method public hidebysig virtual instance bool
+            // Equals(object o) cil managed
+            // {
+            //// Code size       13 (0xd)
+            //.maxstack  2
+            //.locals init (bool V_0)
+            //IL_0000:  nop
+            //IL_0001:  ldarg.0
+            //IL_0002:  ldarg.1
+            //IL_0003:  call instance bool[mscorlib] System.Object::Equals(object)
+            //IL_0008:  stloc.0
+            //IL_0009:  br.s IL_000b
+
+            //IL_000b:  ldloc.0
+            //IL_000c:  ret
+            //} // end of method VirtualCallRecursive::Equals
+
+
+            /// If we change 
+            /// IL_0003:  call 
+            /// by 
+            /// IL_0003:  callvirt
+            /// results in infinite loop 
+            
+        }
+
+
+        public static void Main() {
+            Console.WriteLine(new VirtualCallRecursive().Equals(new VirtualCallRecursive())); // false, 
+            // Equals return object.Equals which compares identity
+        }
+    }
+}
---
+++ Semana 11/Aula 26 e 27/VirtualCall/2-Virtual-Call/VirtualCall1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Aulas2526.VirtualCall1
+{
+    class A {
+        public virtual void M1() {
+            Console.WriteLine("virtual A::M1");
+        }
+    }
+
+    class B : A
+    {
+        public void M2()
+        {
+            Console.WriteLine("B::M2");
+        }
+        //public override void M1()
+        //{
+        //    Console.WriteLine("virtual B::M1 - override A::M1");
+        //}
+
+    }
+
+    class C : B
+    {
+        public override void M1()
+        {
+            Console.WriteLine("virtual C::M1 - override A::M1");
+        }
+        public void M3()
+        {
+            Console.WriteLine("C::M3");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main1()
+        {
+            // REGRA: Primeiro, para decidir se o despacho é estático ou 
+            // dinâmico, o AVE vai verificar se o método definido
+            // no *tipo da referência* é virtual. Se for virtual,
+            // faz callvirt e escolhe o método presente na tabela
+            // do *tipo exato* do objeto (this) - e *não* a tabela do 
+            // *tipo da referência*.
+            A c = new C();
+            c.M1();  // C::M1, Despacho dinâmico
+            //c.M2(); // Erro, Tipo A nao contem M2
+            ((B)c).M2(); // <=> B aux = (B)c; aux.M2();
+            // Tipo da referência: B; B::M2 é virtual? Não => despacho estático
+            // invoca B::M2
+
+            B c2 = (B)c;
+            c2.M2(); // Despacho estatico
+
+            A b = new B();
+            b.M1(); // B::M1 <=> A::M1, Despacho dinâmico
+            // COMENTAR/DESCOMENTAR virtual B::M1
+
+        }
+    }
+}
+/*
+virtual C::M1 - override A::M1
+virtual A::M1
+
+*/ 
