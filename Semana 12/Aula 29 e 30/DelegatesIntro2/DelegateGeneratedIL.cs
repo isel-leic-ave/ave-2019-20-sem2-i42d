82215ad66c1cb56336693b7344b0e276e3ff4409
Nuno Leite <nunocsleite@gmail.com>
2020-05-18 16:58:03 +0100

    Week 12 - Lecture 29-30

---
+++ Semana 12/Aula 29 e 30/DelegatesIntro2/DelegateGeneratedIL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Webao.Test.Dto;
+
+namespace DelegatesIntro
+{
+    public delegate char MyDelegate(char c);
+
+    public class DelegateGeneratedIL
+    {
+        public static char ToUpperCase(char ch) {
+            if (ch >= 'a' && ch <= 'z')
+            {
+                //return (char)(ch - ' ');
+                // Or:
+                return (char)(ch - 'a' + 'A');
+
+            }
+            return ch;
+        }
+
+        public static void Main() {
+
+            /*
+            string methodName = "Webao.Test.Dto.DtoSearch.GetArtistsList";
+            // Neste caso GetArtistsList é o nome do método da classe 
+            // DtoSearch que faz: return this.Results.ArtistMatches.Artist;.
+
+            //
+            // C# hard-coded equivalente ao IL a gerar
+            //
+            DtoResults results = new DtoResults();
+            results.ArtistMatches = new DtoArtistMatches();
+            results.ArtistMatches.Artist = new List<Artist>();
+            Artist artist = new Artist();
+            artist.Name = "xpto artist";
+            results.ArtistMatches.Artist.Add(artist);
+
+            Webao.Test.Dto.DtoSearch obj = new Webao.Test.Dto.DtoSearch();
+            // Set DtoResults in obj
+            obj.Results = results;
+            Func<List<Artist>> myDelegate = obj.GetArtistsList;
+            ///
+            /// TO DO
+            /// Or, define a Func delegate that returns an object --> no generics involved
+
+            // Invoke delegate
+            //myDelegate.Invoke();
+            // Or:
+            List<Artist> list = myDelegate();
+            Console.WriteLine(list[0].Name);
+            */
+
+            MyDelegate del = ToUpperCase;
+
+            Console.WriteLine(del('a'));
+
+        }
+    }
+}
---
+++ Semana 12/Aula 29 e 30/DelegatesIntro2/DtoSearch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Webao.Test.Dto
+{
+    public class DtoSearch
+    {
+        public DtoResults Results { get; set; }
+        // Added
+        public List<Artist> GetArtistsList()
+        {
+            return this.Results.ArtistMatches.Artist;
+        }
+    }
+
+    public class DtoResults
+    {
+        public DtoArtistMatches ArtistMatches { get; set; }
+    }
+
+    public class DtoArtistMatches
+    {
+        public List<Artist> Artist { get; set; }
+    }
+}
